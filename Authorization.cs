72e70aff46a13e232ccd5529f3d249bfa1f93a23
iMprov <improv00@mail.ru>
2025-04-20 17:00:07 +0700

    Подключение библиотек в коде

--- Authorization.cs
+++ Authorization.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
+using Dapper;
+using System.IO;
 
 
 namespace Household_book
